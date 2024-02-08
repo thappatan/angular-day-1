9f672f5823beabd6e47006d9d1f36d4ab3f70dbd
DEVSUN <thappatanchanphen@gmail.com>
2024-02-08 03:39:50 +0000

    edit: add loop and userModel

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserModel } from './models/user.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   name: string = "";
   count: number = 0;
   isShow: boolean = true;
   changeColor: boolean = true;
+  userList: string[] = ["user1", "user2", "user3", "user4"];
+  userListByModel: UserModel[] = [
+    { firstname: "user 1", lastname: "user 1", username: "user 1", age: 20 }, 
+    { firstname: "user 2", lastname: "user 2", username: "user 2", age: 26 }
+  ];
 
   ngOnInit(): void {
     console.log("component inited")
@@ -17,19 +24,19 @@ export class AppComponent implements OnInit{
     this.count = 2;
   }
 
-  changeColorContent(){
+  changeColorContent() {
     this.changeColor = !this.changeColor;
   }
 
-  showHideContent(){
+  showHideContent() {
     this.isShow = !this.isShow;
   }
 
-  plusCount(){
+  plusCount() {
     this.count++;
   }
 
-  minusCount(){
+  minusCount() {
     this.count--;
   }
 }
---
+++ first-project/src/app/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface UserModel {
+    firstname: string;
+    lastname: string;
+    username: string;
+    age: number;
+}
